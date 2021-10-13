1aa9abd3ee9afc176c8b41b8ed51bfbad85c3a27
“electricganesha” <christian.marques@gmail.com>
2021-10-13 12:45:07 +0100

    Add Spinner on page change

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,32 @@
-import  React from 'react';
+import React, { useState } from 'react';
+import Router from 'next/router'
 import Head from "next/head";
+import Spinner from '../components/Spinner';
 import Footer from '../components/Footer';
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  Router.events.on('routeChangeStart', () => {
+    setIsLoading(true);
+  });
+  Router.events.on('routeChangeComplete', () => {
+    setIsLoading(false);
+  });
+
   return <React.Fragment>
     <Head>
-        <title>A Liberdade N&atilde;o &Eacute; Um Quiz</title>
-        <meta name="description" content="A Liberdade N&atilde;o &Eacute; Um Quiz" />
-        <link rel="icon" href="/favicon.ico" />
+      <title>A Liberdade N&atilde;o &Eacute; Um Quiz</title>
+      <meta name="description" content="A Liberdade N&atilde;o &Eacute; Um Quiz" />
+      <link rel="icon" href="/favicon.ico" />
     </Head>
-    <Component {...pageProps} />
+    {isLoading ? <Spinner /> :
+      <Component {...pageProps} />
+    }
     <Footer />
-    </React.Fragment>;
+  </React.Fragment>;
 }
 
 export default MyApp;
