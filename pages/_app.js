009bbe60b3f2ead63257944fc83bb98616dbde7c
“electricganesha” <christian.marques@gmail.com>
2022-01-06 12:21:30 +0000

    Add gtag script

--- pages/_app.js
+++ pages/_app.js
@@ -21,9 +21,11 @@ function MyApp({ Component, pageProps }) {
       <title>A Liberdade N&atilde;o &Eacute; Um Quiz</title>
       <meta name="description" content="A Liberdade N&atilde;o &Eacute; Um Quiz" />
       <link rel="icon" href="/favicon.ico" />
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"/> 
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-EMYYH0VLLZ"></script>
+      <script dangerouslySetInnerHTML={{__html: `window.dataLayer = window.dataLayer || []; function gtag(){window && window.dataLayer && dataLayer.push(arguments)}  gtag('js', new Date()); gtag('config', 'G-EMYYH0VLLZ');`}}/>
     </Head>
     {isLoading ? <Spinner /> :
       <Component {...pageProps} />
